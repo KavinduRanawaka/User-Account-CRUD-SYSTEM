8f9c7f6132509972cbe9c9153574445867d5cce3
Kavindu Ranawaka <knranawaka@std.appsc.sab.ac.lk>
2024-08-28 13:48:27 +0530

    Create User.js

---
+++ frontend/src/UserDetails/User.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from '../Nav/Nav';
+
+function Users() {
+  return (
+    <div>
+      <Nav/>
+      <h1>User Details Display page</h1>
+    </div>
+  )
+}
+
+export default Users

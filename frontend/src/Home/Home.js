389654529e956b3c8a7ce605f92a30274815fb34
Kavindu Ranawaka <knranawaka@std.appsc.sab.ac.lk>
2024-08-28 13:47:23 +0530

    Create Home.js

---
+++ frontend/src/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from '../Nav/Nav';
+
+function Home() {
+  return (
+    <div>
+      <Nav></Nav>
+      <h1>Homes</h1>
+    </div>
+  )
+}
+
+export default Home

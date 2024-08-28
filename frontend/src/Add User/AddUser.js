6c4e0cb51a25b269b6c35d21c8a4fdea389e0d2c
Kavindu Ranawaka <knranawaka@std.appsc.sab.ac.lk>
2024-08-28 13:46:56 +0530

    Create AddUser.js

---
+++ frontend/src/Add User/AddUser.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from '../Nav/Nav';
+
+function AddUser() {
+  return (
+    <div>
+      <Nav/>
+      <h1>Add User</h1>
+    </div>
+  )
+}
+
+export default AddUserimport 
